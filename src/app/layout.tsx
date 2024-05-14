ba0b95fe96265e116db9aff4a6bc21f923b76822
Soudeh <babaeizadeh.s@gmail.com>
2024-05-14 21:20:09 +0330

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Link from "next/link";
+import { ModeToggle } from "@/components/DarkModeToggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider 
+          attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange>     
+            <main className="flex min-h-screen flex-col items-center p-24">
+              <div className=" flex z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
+                <Link href="/"><h2 className="text-2xl text-bold">PokemonFinder</h2></Link>
+                <div className='flex gap-18'>
+                  <ModeToggle/>
+              </div>
+              </div>            
+              <div className="mt-10">
+                 {children}
+              </div>
+          </main>     
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
