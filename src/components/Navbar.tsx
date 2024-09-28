943053ca06a7e6e51ec438c5f5640a585181428a
Soudeh <babaeizadeh.s@gmail.com>
2024-09-29 00:04:13 +0330

    update pokemon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,9 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between flex-auto">
       {" "}
-      <Link href="/" className="cursor-pointer">
+      <Link href="/" className="cursor-pointer flex items-center">
         <Image src="/poki.png" alt="Logo" width={60} height={60} />
+        <p className="font-semibold">Pokemon</p>
       </Link>
       <ModeToggle />
     </div>
